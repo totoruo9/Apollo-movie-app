8dd7ddcda2074fe289a1c694e0cc24ca38bd255d
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-09-08 16:25:47 +0900

    [Set Apollo]

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import {HashRouter as Router, Route} from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
+  return (  
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
   );
 }
 
